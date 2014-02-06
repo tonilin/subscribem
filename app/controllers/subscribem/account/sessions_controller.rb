347676ec2cbacc2f25fbfb5bd5b6c76f5487dfc6
tonilin <tonilin@gmail.com>
2014-02-06 15:54:58 +0800

    login

--- app/controllers/subscribem/account/sessions_controller.rb
+++ app/controllers/subscribem/account/sessions_controller.rb
@@ -5,5 +5,13 @@ module Subscribem
     def new
       @user = User.new
     end
+
+    def create
+      if env["warden"].authenticate(:scope => :user)
+          flash[:notice] = "You are now signed in."
+          redirect_to root_path
+      end
+    end
+
   end
 end
