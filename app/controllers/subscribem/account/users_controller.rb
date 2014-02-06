57bf4d0690874af0a8d98a13080b6243953cdbf2
tonilin <tonilin@gmail.com>
2014-02-06 16:58:27 +0800

    apply signup

--- app/controllers/subscribem/account/users_controller.rb
+++ app/controllers/subscribem/account/users_controller.rb
@@ -1,11 +1,24 @@
 require_dependency "subscribem/application_controller"
 
 module Subscribem
-  class Account::UsersController < ApplicationController
+  class Account::UsersController < Subscribem::ApplicationController
     def new
       @user = Subscribem::User.new
     end
 
+    def create
+      account = Subscribem::Account.find_by(:subdomain => request.subdomain)
+      user = account.users.create(user_params)
+      force_authentication!(account, user)
+      flash[:success] = "You have signed up successfully."
+      redirect_to root_path
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 
   end
 end
