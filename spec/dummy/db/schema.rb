308aae09984449d2681b987fbc4de6e1dbd00ef5
tonilin <tonilin@gmail.com>
2014-02-06 11:39:05 +0800

    add owner_id to account

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206032314) do
+ActiveRecord::Schema.define(version: 20140206033828) do
 
   create_table "subscribem_accounts", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "owner_id"
   end
 
 end
