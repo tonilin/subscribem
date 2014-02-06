fd181432f8a6a224735159668570facf4ca3f103
tonilin <tonilin@gmail.com>
2014-02-06 15:28:26 +0800

    dashboard required login

--- app/controllers/subscribem/account/dashboard_controller.rb
+++ app/controllers/subscribem/account/dashboard_controller.rb
@@ -1,6 +1,7 @@
 require_dependency "subscribem/application_controller"
 
 module Subscribem
-  class Account::DashboardController < ApplicationController
+  class Account::DashboardController < Subscribem::ApplicationController
+    before_filter :authenticate_user!
   end
 end
