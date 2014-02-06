0a4974546b8bb0ec5bc4d1385aaf3aa09fce2d8d
tonilin <tonilin@gmail.com>
2014-02-06 14:37:38 +0800

    only allow valid char

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -7,6 +7,7 @@ module Subscribem
 
     EXCLUDED_SUBDOMAINS = %w(admin)
     validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS, :message => "is not allowed. Please choose another subdomain."
+    validates_format_of :subdomain, :with => /\A[\w\-]+\Z/i, :message => "is not allowed. Please choose another subdomain."
 
     before_validation do
       self.subdomain = subdomain.to_s.downcase
