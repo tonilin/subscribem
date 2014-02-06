4baaed4b1eaf92777cae38effee75e6635b4ef17
tonilin <tonilin@gmail.com>
2014-02-06 14:25:51 +0800

    validates subdomain for uniquness and presence

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -3,6 +3,8 @@ module Subscribem
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
 
+    validates :subdomain, :presence => true, :uniqueness => true
+
 
   end
 end
