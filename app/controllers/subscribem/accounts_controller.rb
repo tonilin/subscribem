42a1dcb2e76f0443aa20368ab25352500840736b
tonilin <tonilin@gmail.com>
2014-02-06 11:21:17 +0800

    implements account#new

--- app/controllers/subscribem/accounts_controller.rb
+++ app/controllers/subscribem/accounts_controller.rb
@@ -2,5 +2,10 @@ require_dependency "subscribem/application_controller"
 
 module Subscribem
   class AccountsController < ApplicationController
+
+    def new
+      @account = Subscribem::Account.new
+    end
+
   end
 end
