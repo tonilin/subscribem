b23a96a16e90cd4b4f31bb08d36aa7453f1781ee
tonilin <tonilin@gmail.com>
2014-02-06 16:49:23 +0800

    add route to do user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Subscribem::Engine.routes.draw do
       get "/sign_in", :to => "sessions#new"
       post "/sign_in", :to => "sessions#create", :as => :sessions
       get "/sign_up", :to => "users#new", :as => :user_sign_up
+      post "/sign_up", :to => "users#create", :as => :do_user_sign_up
     end
 
   end
