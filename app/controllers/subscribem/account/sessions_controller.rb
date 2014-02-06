23f29e4e25b0325add57aa11ceea9402330df632
tonilin <tonilin@gmail.com>
2014-02-06 15:36:29 +0800

    add new action

--- app/controllers/subscribem/account/sessions_controller.rb
+++ app/controllers/subscribem/account/sessions_controller.rb
@@ -1,6 +1,9 @@
 require_dependency "subscribem/application_controller"
 
 module Subscribem
-  class Account::SessionsController < ApplicationController
+  class Account::SessionsController < Subscribem::ApplicationController
+    def new
+      @user = User.new
+    end
   end
 end
