cb204766871ae0ca058f7bd8400dc05ad63d8b02
tonilin <tonilin@gmail.com>
2014-02-06 11:32:27 +0800

    fill in email password

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,6 +5,10 @@ feature 'Accounts' do
     visit subscribem.root_path
     click_link "Account Sign Up"
     fill_in "Name", :with => "Test"
+    fill_in "Email", :with => "subscribem@example.com"
+    fill_in 'Password', :with => "password", :exact => true
+    fill_in 'Password confirmation', :with => "password"
+
     click_button "Create Account"
     success_message = "Your account has been successfully created."
     page.should have_content(success_message)
