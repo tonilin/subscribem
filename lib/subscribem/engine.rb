85fc0d3ed9f074dad28352be4fa52241caaa4e94
tonilin <tonilin@gmail.com>
2014-02-06 11:49:38 +0800

    add warden initializer

--- lib/subscribem/engine.rb
+++ lib/subscribem/engine.rb
@@ -9,5 +9,18 @@ module Subscribem
       g.test_framework :rspec, :view_specs => false
     end
 
+    initializer "subscribem.middleware.warden" do
+      Rails.application.config.middleware.use Warden::Manager do |manager|
+        manager.serialize_into_session do |user|
+          user.id
+        end
+        manager.serialize_from_session do |id|
+          Subscribem::User.find(id)
+        end
+      end
+    end
+
+
+
   end
 end
