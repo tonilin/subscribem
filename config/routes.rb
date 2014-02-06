b72db9385dcf75038133312e51d7cb2375a490dc
tonilin <tonilin@gmail.com>
2014-02-06 11:27:13 +0800

    routing to post accounts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Subscribem::Engine.routes.draw do
   root "dashboard#index"
 
   get '/sign_up', :to => "accounts#new", :as => :sign_up
+  post '/accounts', :to => "accounts#create", :as => :accounts
 
 
 end
