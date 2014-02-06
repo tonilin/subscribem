947abd32b14d75e13b514e09e6bb5163a8a29324
tonilin <tonilin@gmail.com>
2014-02-06 17:45:54 +0800

    extract application methods

--- lib/subscribem/engine.rb
+++ lib/subscribem/engine.rb
@@ -24,6 +24,15 @@ module Subscribem
     end
 
 
+    config.to_prepare do
+      root = Subscribem::Engine.root
+      extenders_path = root + "app/extenders/**/*.rb"
+      Dir.glob(extenders_path) do |file|
+        Rails.configuration.cache_classes ? require(file) : load(file)
+      end
+    end
+
+
 
   end
 end
