67c196bd1b775bbc0c6f9be97f2a76cdab4e2b79
tonilin <tonilin@gmail.com>
2014-02-06 14:47:55 +0800

    require factory girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require "capybara/rspec"
+require "factory_girl"
 
 
 # Requires supporting ruby files with custom matchers and macros, etc,
